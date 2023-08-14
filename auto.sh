e7937a47e198cea6f68707469946734071abe488
tha dddd <99595273+thadddd@users.noreply.github.com>
2023-08-14 06:52:46 +0000

    Add files via upload
    Fresh Start

---
+++ auto.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+cd /home/xmrig || return
+sudo ./xmrig
---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+### script to move mine code into correct folders 
+
+cd home || return
+sudo git clone https://github.com/thadddd/xmrig.git
+cd /home/xmrig || return
+
+sudo chmod +x xmrig
+sudo chmod +x auto.sh
+
+sudo mv xmrigstart.service /etc/system.d/system
+
+sudo systemctl enable xmrigstart.service
+sudo systemctl start xmrigstart.service
+
+sudo reboot
