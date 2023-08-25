1096095c1c0b5a5f8558e2e1cde27a043a833245
tha dddd <99595273+thadddd@users.noreply.github.com>
2023-08-25 22:38:19 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,18 +4,17 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 
-### script to move mine code into correct folders 
-cd /home/xmrig
-sudo apt install -y monitorix screen aptitude 
-sudo systemctl enable monitorix 
-sudo systemctl start monitorix
-
-screen -S mine
+cd /home/xmrig || return
 
 sudo chmod +x xmrig
-sudo chmod +x auto.sh
+sudo chmod +x autostart.sh
 
-sudo mv xmrigstart.service /etc/systemd/system
+sudo apt install monitorix screen aptitude -y
+
+sudo systemctl enable monitorix
+sudo systemctl start monitorix
+
+sudo cp xmrigstart.service /etc/systemd/system/xmrigstart.service
 
 sudo systemctl enable xmrigstart.service
 sudo systemctl start xmrigstart.service
